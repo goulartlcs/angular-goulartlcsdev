b5f2e551f0fa79a4161b043e63a88bdaac99d9c4
goulartlcs <lucas@goulartlcs.dev>
2022-04-21 14:17:43 -0300

    Ajuste de layout da secao de projetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Project } from './core/models/project.model';
-import { SectionProjectsService } from './core/services/section-projects.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-
-  constructor(private projectsService: SectionProjectsService) { }
-
-  projects: Project[] = [];
-
-  ngOnInit() {
-    this.projectsService.getProjects().then(res => this.projects = res);
-  }
-
-}
+export class AppComponent {}
 
