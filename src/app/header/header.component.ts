eabc0a1f810046b7be91a0640bc40a924fb334a3
goulartlcs <lucas@goulartlcs.dev>
2022-04-21 15:04:17 -0300

    Alterado a versao do projeto

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  active: Boolean = false;
+  menuClicked: String = 'Início';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handlerMenuClicked(evt: any) {
+    this.menuClicked = evt.target.innerHTML;
   }
 
 }
